4999ac6afbc98175b453874a46860c55fb0ca247
LouisWongHK <LouisWongHK@protonmail.com>
2024-11-24 22:16:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -14,34 +14,18 @@
 // });
 
 const http = require('http');
-const fs = require('fs');
-const path = require('path');
 
 const hostname = '0.0.0.0';
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-    // Resolve the file path to the index.html file
-    const filePath = path.join(__dirname, 'index.html');
-    
-    // Read the index.html file
-    fs.readFile(filePath, (err, content) => {
-        if (err) {
-            // Handle errors by returning a 500 Internal Server Error
-            res.statusCode = 500;
-            res.setHeader('Content-Type', 'text/plain');
-            res.end('Internal Server Error\n');
-        } else {
-            // Serve the index.html file with a 200 status code
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/html');
-            res.end(content);
-        }
-    });
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, world!\n');
 });
 
-// Start the server and listen on the specified port and hostname
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
+
 
